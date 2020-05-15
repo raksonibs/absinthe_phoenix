4c13805c91c88d33f0972fc85a1458754c1bac9a
Oskar Niburski <oskarniburski@gmail.com>
2020-05-15 14:23:07 -0700

    fix version

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Absinthe.Phoenix.Mixfile do
   defp deps do
     [
       {:absinthe_plug, "~> 1.5.0"},
-      {:absinthe, "~> 1.5.0"},
+      {:absinthe, "~> 1.5.0-rc.4"},
       {:decimal, "~> 1.0"},
       {:phoenix_pubsub, "~> 2.0"},
       {:phoenix, "~> 1.5.0"},
